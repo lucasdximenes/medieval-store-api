4fbd7028bcfeed8449eb0376e2662599998fe302
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-05 17:40:51 -0300

    feat: :rocket: create product service with `create` function

---
+++ src/services/product.service.ts
@@ -0,0 +1,16 @@
+import connection from '../models/connection';
+import ProductModel from '../models/product.model';
+import { CreateProduct, CreatedProduct } from '../interfaces/product.interface';
+
+export default class ProductService {
+  private productModel: ProductModel;
+
+  constructor() {
+    this.productModel = new ProductModel(connection);
+  }
+
+  public create = async (product: CreateProduct): Promise<CreatedProduct> => {
+    const createdProduct = await this.productModel.create(product);
+    return createdProduct;
+  };
+}
