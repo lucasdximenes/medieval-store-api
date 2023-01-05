9d2d446734836da6e5b2a01882e6385b3347ea8e
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-05 17:50:13 -0300

    feat: :bug: Change validation error messages

--- src/middlewares/validations/schemas/validationSchemas.ts
+++ src/middlewares/validations/schemas/validationSchemas.ts
@@ -1,15 +1,15 @@
 import Joi from 'joi';
 
 export const amountSchema = Joi.string().min(3).required().messages({
-  'string.base': 'amount must be a string', // 400
-  'string.empty': 'amount is required', // 422
-  'string.min': 'amount must be at least 3 characters long', // 422
+  'string.base': '"amount" must be a string',
+  'string.empty': '"amount" is required',
+  'string.min': '"amount" length must be at least 3 characters long',
 });
 
 export const nameSchema = Joi.string().min(3).required().messages({
-  'string.base': 'name must be a string', // 400
-  'string.empty': 'name is required', // 422
-  'string.min': 'name must be at least 3 characters long', // 422
+  'string.base': '"name" must be a string',
+  'string.empty': '"name" is required',
+  'string.min': '"name" length must be at least 3 characters long',
 });
 
 export const newProductSchema = Joi.object({
