84ca6864ccabb825195430219a7371bdf3f5286d
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-05 17:43:34 -0300

    feat: :rocket: adds use of routes and error middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import errorMiddleware from './middlewares/error.middleware';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+app.use(errorMiddleware);
 
 export default app;
