34197c4011a4775d96ce854830130b0eb8bc64ae
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-05 18:10:58 -0300

    feat: :rocket: adds `getAll` function to product service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,6 +1,6 @@
 import connection from '../models/connection';
 import ProductModel from '../models/product.model';
-import { CreateProduct, CreatedProduct } from '../interfaces/product.interface';
+import { CreateProduct, CreatedProduct, Product } from '../interfaces/product.interface';
 
 export default class ProductService {
   private productModel: ProductModel;
@@ -12,5 +12,10 @@ export default class ProductService {
   public create = async (product: CreateProduct): Promise<CreatedProduct> => {
     const createdProduct = await this.productModel.create(product);
     return createdProduct;
+  };
+
+  public getAll = async (): Promise<Product[]> => {
+    const products = await this.productModel.getAll();
+    return products;
   };
 }
