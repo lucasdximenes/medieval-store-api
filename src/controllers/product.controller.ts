abf93487d18335f018fca7d0551c1333119fe310
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-05 17:41:28 -0300

    feat: :rocket: create product controller with `create` function

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import statusCodes from '../constants/statusCodes';
+import ProductService from '../services/product.service';
+
+export default class ProductController {
+  private productService: ProductService;
+
+  constructor() {
+    this.productService = new ProductService();
+  }
+
+  public create = async (req: Request, res: Response): Promise<Response> => {
+    const newProduct = req.body;
+    const createdProduct = await this.productService.create(newProduct);
+    return res.status(statusCodes.CREATED).json(createdProduct);
+  };
+}
