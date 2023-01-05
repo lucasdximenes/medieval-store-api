3adedf62dec7e1d8e03b7a57a8157df888796b9f
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-05 19:15:29 -0300

    feat: :building_construction: adds user interfaces

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,13 @@
+export interface NewUser {
+  username: string;
+  vocation: string;
+  level: number;
+  password: string;
+}
+
+export interface User {
+  id: number;
+  username: string;
+  vocation: string;
+  level: number;
+}
