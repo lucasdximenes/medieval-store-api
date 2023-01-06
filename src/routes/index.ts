79ddfc7297a0ad6b1eba834ef5c2326fa2448941
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-06 20:06:39 -0300

    feat: :rocket: adds GET `order` route to return all orders

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import productRoutes from './product.routes';
 import userRoutes from './user.routes';
+import orderRoutes from './order.routes';
 
 const router = Router();
 
 router.use('/products', productRoutes);
 router.use('/users', userRoutes);
+router.use('/orders', orderRoutes);
 
 export default router;
