ec9c21cf6a9e5e76ec4c41d628ffab8ad83cff72
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-06 21:18:43 -0300

    feat: :rocket: add `login` function in user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -14,4 +14,10 @@ export default class UserController {
     const newUserToken = await this.userService.create(newUser);
     return res.status(statusCodes.CREATED).json({ token: newUserToken });
   };
+
+  public login = async (req: Request, res: Response): Promise<Response> => {
+    const { username, password } = req.body;
+    const userToken = await this.userService.login(username, password);
+    return res.status(statusCodes.OK).json({ token: userToken });
+  };
 }
