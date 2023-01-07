6224569a6facb7bc2377c38a0d9e2d6d140fa21d
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-06 21:18:21 -0300

    feat: :rocket: adds `login` function in user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken';
 import connection from '../models/connection';
 import UserModel from '../models/user.model';
-import { User, NewUser } from '../interfaces/user.interface';
+import { NewUser, SanitizedUser } from '../interfaces/user.interface';
+import { UnauthorizedError } from '../errors';
 
 const { JWT_SECRET } = process.env;
 
@@ -12,13 +13,26 @@ export default class UserService {
     this.userModel = new UserModel(connection);
   }
 
-  private generateToken = (payload: User): string => {
+  private generateToken = (payload: SanitizedUser): string => {
     const token = jwt.sign(payload, JWT_SECRET as string, { expiresIn: '1d' });
     return token;
   };
 
   public create = async (newUser: NewUser): Promise<string> => {
     const user = await this.userModel.create(newUser);
+    const token = this.generateToken(user);
+    return token;
+  };
+
+  public login = async (username: string, password: string): Promise<string> => {
+    const user = await this.userModel.getByUsername(username);
+
+    if (!user || user.password !== password) {
+      throw new UnauthorizedError('Username or password invalid');
+    }
+
+    delete user.password;
+
     const token = this.generateToken(user);
     return token;
   };
