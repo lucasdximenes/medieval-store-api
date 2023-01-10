3e980896d90b200cca816ff6486653790d99541d
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-10 17:55:06 -0300

    feat: :rocket: create POST route for insert new orders `/orders`

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import OrderController from '../controllers/order.controller';
+import { validateNewOrder } from '../middlewares/validations/validation.middlewares';
+import authMiddleware from '../middlewares/auth.middleware';
 
 const router = Router();
 
 const orderController = new OrderController();
 
 router.get('/', orderController.getAll);
+
+router.post('/', authMiddleware, validateNewOrder, orderController.insert);
 
 export default router;
