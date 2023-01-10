a181362b1ed2d99a98b416e1290c233faa049450
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-10 17:52:42 -0300

    feat: :rocket: create `insert` function that creates a new order

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,16 +1,53 @@
 import connection from '../models/connection';
 import OrderModel from '../models/order.model';
-import { Order } from '../interfaces/order.interface';
+import { Order, NewOrder } from '../interfaces/order.interface';
+import ProductModel from '../models/product.model';
+import UserModel from '../models/user.model';
+import { NotFoundError, BadRequestError } from '../errors';
 
 export default class OrderService {
   private orderModel: OrderModel;
 
+  private productModel: ProductModel;
+
+  private userModel: UserModel;
+
   constructor() {
     this.orderModel = new OrderModel(connection);
+    this.productModel = new ProductModel(connection);
+    this.userModel = new UserModel(connection);
   }
 
   public getAll = async (): Promise<Order[]> => {
     const orders = await this.orderModel.getAll();
     return orders;
+  };
+
+  private isAValidOrder = async (order: NewOrder): Promise<void> => {
+    const { userId, productsIds } = order;
+
+    const userExists = await this.userModel.getById(userId);
+    if (Object.keys(userExists).length === 0) throw new NotFoundError('User not found');
+
+    const allProductsExist = await this.productModel.allProductsExist(productsIds);
+    if (!allProductsExist) throw new NotFoundError('Product not found');
+
+    const allProductsAreAvailable = await this.productModel.allProductsAreAvailable(
+      productsIds,
+    );
+    if (!allProductsAreAvailable) {
+      throw new BadRequestError('Some products are unavailable');
+    }
+  };
+
+  public insert = async (order: NewOrder): Promise<NewOrder> => {
+    const { userId, productsIds } = order;
+
+    await this.isAValidOrder(order);
+
+    const orderId = await this.orderModel.insert(userId);
+    await this.productModel.update(productsIds, orderId);
+
+    return { userId, productsIds };
   };
 }
