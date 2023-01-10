d17f5e1f0fff85be37717cadb7128d9ff2268f1b
Lucas Ximenes <lucasdximenes@gmail.com>
2023-01-10 17:53:27 -0300

    feat: :rocket: create `insert` function in order controller

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -13,4 +13,14 @@ export default class OrderController {
     const orders = await this.orderService.getAll();
     return res.status(statusCodes.OK).json(orders);
   };
+
+  public insert = async (req: Request, res: Response): Promise<Response> => {
+    const {
+      user: { id },
+      productsIds,
+    } = req.body;
+    const newOrder = { userId: id, productsIds };
+    const createdOrder = await this.orderService.insert(newOrder);
+    return res.status(statusCodes.CREATED).json(createdOrder);
+  };
 }
